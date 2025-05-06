b652ab86cfc4b75ec9893ec193cf3138125e9e8a
Dev-JoaoVictor <joaovictor.ramos34@gmail.com>
2025-05-05 22:35:01 -0300

    feat(verificacao de usuario): adiciona verificacao de usuario

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { DeliveriesController } from "@/controllers/deliveries-controller";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { verifyUserAuthorizarion } from "@/middlewares/verifyUserAuthorization";
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
 
-deliveriesRoutes.use(ensureAuthenticated);
+deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorizarion(["sale"]));
 deliveriesRoutes.post("/", deliveriesController.create);
 
 export { deliveriesRoutes };
