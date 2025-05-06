5d6fc65956949c56b82c8957c7a2fc21ab1261a7
Dev-JoaoVictor <joaovictor.ramos34@gmail.com>
2025-05-06 20:13:53 -0300

    feat(controller deliverie): adiciona a controller de deliveries e status

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -2,11 +2,16 @@ import { Router } from "express";
 import { DeliveriesController } from "@/controllers/deliveries-controller";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 import { verifyUserAuthorizarion } from "@/middlewares/verifyUserAuthorization";
+import { DeliveriesStatusController } from "@/controllers/deliveries-status-controller";
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
+const deliveriesStatusController = new DeliveriesStatusController();
 
-deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorizarion(["sale"]));
 deliveriesRoutes.post("/", deliveriesController.create);
+deliveriesRoutes.get("/", deliveriesController.index);
+deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorizarion(["sale"]));
+
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update);
 
 export { deliveriesRoutes };
